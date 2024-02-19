d2890fa737de9b0c33958d63962e883aaa17f662
kimchosule <chosule@naver.com>
2024-02-19 21:44:36 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+    let sum = parseInt(a.toString() + b.toString());
+    let multiply = 2 * a * b;
+    if(sum >= multiply){
+        return sum;
+    }else{ 
+    } return multiply
+}
